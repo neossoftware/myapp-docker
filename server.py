bc01c4274ff0eae51c5a8b84a76e3411a5f6a403
neossoftware <mario.hidalgo@yahoo.com.mx>
2022-07-08 21:19:59 -0500

    bad

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        title = "Hello, World"
-        bgcolor = "limegreen"
+        title = "Oops!!!"
+        bgcolor = "darkred"
         self.render("template.html", title= title, bgcolor = bgcolor)
         print(self.request)
 
